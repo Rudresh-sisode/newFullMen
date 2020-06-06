8e5c73e489bf8cc4967a2fc03479032fc1299a23
Sohame Rajput <rudranrajput@gmail.com>
2020-06-06 12:53:37 +0530

    mime type has been added

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from "@angular/core";
-import { NgForm } from "@angular/forms";
+import { Validators, FormGroup, FormControl } from "@angular/forms";
 
 import { Post } from '../post.model';
 import { PostsService } from '../post.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import { mimeType } from './mime-type.validator';
 
 @Component({
   selector: "app-post-create",
@@ -13,15 +14,22 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
 export class PostCreateComponent implements OnInit {
   enteredTitle = "";
   enteredContent = "";
+  form:FormGroup;
   private mode='create';
   private postId:string;
   isLoading=false;
   post:Post;
+  imagePreview:string;
 
 
   constructor(public postsService:PostsService, public route:ActivatedRoute){}
 
   ngOnInit(){
+    this.form=new FormGroup({
+      title:new FormControl(null,{validators:[Validators.required, Validators.minLength(3)]}),
+      content:new FormControl(null,{validators:[Validators.required, Validators.minLength(4) ]}),
+      image:new FormControl(null, {validators:[Validators.required],asyncValidators:[mimeType]})
+    })
     this.route.paramMap.subscribe((paramMap:ParamMap)=>{
       if(paramMap.has('postId')){
         this.mode='edit';
@@ -34,6 +42,7 @@ export class PostCreateComponent implements OnInit {
            title:postData.title,
            content:postData.content
          }
+         this.form.setValue({'title':this.post.title,'content':this.post.content});
        });
       }
       else{
@@ -43,19 +52,30 @@ export class PostCreateComponent implements OnInit {
     })
   }
 
-  onSavePost(form: NgForm) {
-    if (form.invalid) {
+  onImagePicked(event:Event){
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({image:file});
+    this.form.get('image').updateValueAndValidity();
+    const reader=new FileReader();
+    reader.onload=()=>{
+      this.imagePreview= reader.result as string;
+    }
+    reader.readAsDataURL(file);
+  }
+
+  onSavePost() {
+    if (this.form.invalid) {
       return;
     }
     this.isLoading=true;
     if(this.mode=== 'create'){
-      this.postsService.addPost( form.value.title,form.value.content);
+      this.postsService.addPost( this.form.value.title,this.form.value.content);
     }
     else{
-      this.postsService.updatePost(this.postId,form.value.title,form.value.content)
+      this.postsService.updatePost(this.postId,this.form.value.title,this.form.value.content)
     }
 
-    form.resetForm();
+    this.form.reset();
 
   }
 }
